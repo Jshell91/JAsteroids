b520eb23313a0fbe836f55ac3b3fe36ca0f93a47
Jshell91 <jshell1991@gmail.com>
2025-01-14 02:04:43 +0100

    Game done

---
+++ asteroid.py
@@ -0,0 +1,27 @@
+from circleshape import CircleShape
+import pygame
+import constants
+import random
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)        
+
+    def split(self):
+        self.kill()
+        if(self.radius <= constants.ASTEROID_MIN_RADIUS): return
+        else: 
+            angle = random.uniform(20, 50)
+            vec1 = self.velocity.rotate(angle)
+            vec2 = self.velocity.rotate(angle * -1)
+            radius = self.radius - constants.ASTEROID_MIN_RADIUS
+            ast1 = Asteroid(self.position.x, self.position.y, radius)
+            ast2 = Asteroid(self.position.x, self.position.y, radius)
+            ast1.velocity = vec1 * 1.2
+            ast2.velocity = vec2 * 1.2
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 	pygame.init()
@@ -13,17 +17,49 @@ def main():
 
 	clock = pygame.time.Clock()
 	dt = 0
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+
+	
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	field = AsteroidField()
+	Shot.containers = (shots, updatable, drawable)
+
+	Player.containers = (updatable, drawable)
+
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+	
 
 	while running:
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+			
+
+		for upd in updatable:
+			upd.update(dt)
+		
+		for ast in asteroids:
+			if ast.colliding(player):
+				print("GAME OVER")
+				sys.exit()
+			for shot in shots:
+				if shot.colliding(ast):
+					shot.kill()
+					ast.split()
+
+		
 
 		screen.fill("black")
 
-		player.draw(screen)
+		for draw in drawable:
+			draw.draw(screen)		
 		
 		pygame.display.flip()	
 
